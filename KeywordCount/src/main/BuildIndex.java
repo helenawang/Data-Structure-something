581e99dc9bb3c0100d29d18b40608c8ca2d77c85
Ephemera <helenawang@users.noreply.github.com>
2017-10-08 18:20:09 +0800

    增加了对AC自动机的支持

--- KeywordCount/src/main/BuildIndex.java
+++ KeywordCount/src/main/BuildIndex.java
@@ -4,14 +4,20 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import model.ACA; 
 
 public class BuildIndex {
 	static String[] keywords;
+	/**
+	 * 默认实现
+	 * @param filename
+	 * @return
+	 */
 	public static String[] buildIndexFromFile(String filename) {
 		return buildArray(filename);
 	}
 	
-	private static String[] buildArray(String filename) {
+	public static String[] buildArray(String filename) {
 		long start = System.currentTimeMillis();
 		String contents;
 		try {
@@ -29,9 +35,39 @@ public class BuildIndex {
 		System.out.println("建立索引完成，用时 " + (int)(end - start) + " ms");
 		return keywords;
 	}
+	/**
+	 * ACA自动机实现索引
+	 * @param filename
+	 * @return
+	 */
+	public static ACA buildACA(String filename) {
+		long start = System.currentTimeMillis();
+		String contents;
+		try {
+			contents = new String(Files.readAllBytes(Paths.get(filename)), StandardCharsets.UTF_8);
+			keywords = contents.split("\\s+");//难道我用不是回车符？？从github上粘下来后有问题了
+//			System.out.println(keywords.length);
+//			for(String keyword: keywords) {
+//				System.out.println(keyword);
+//			}
+		} catch (IOException e) {
+			System.err.println("read file [" + filename + "] fail!");
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		ACA aca = new ACA();
+		aca.buildGoto(keywords);
+		aca.buildFailure();
+		System.out.println("建立索引完成，用时 " + (int)(end - start) + " ms");
+		return aca;
+	}
 	public static void main(String[] args) {
 		String filename = "data/keywords.txt";
-		BuildIndex.buildIndexFromFile(filename);
+		ACA aca = BuildIndex.buildACA(filename);
+		int[] results = aca.match("structuresizeof", keywords.length);
+		for(int i=0; i<keywords.length; i++) {
+			System.out.println(keywords[i] + " : " + results[i]);
+		}
 	}
 
 }

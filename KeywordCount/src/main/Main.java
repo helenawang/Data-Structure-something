d0d1fb5e6f891b5a2e4266743723eff42568527e
Ephemera <helenawang@users.noreply.github.com>
2017-10-12 21:17:43 +0800

    增加代码行数统计、总用时计数

--- KeywordCount/src/main/Main.java
+++ KeywordCount/src/main/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 import model.ACA;
 
 public class Main {
+	static int time_ms = 0;
 	//程序入口
 	public static void main(String[] args) {
 		String sourcefile = "data/merge.c";
@@ -17,7 +18,7 @@ public class Main {
 		
 		//建立关键字查找结构
 		String[] keywords;
-		BuildIndex.buildArray(keywordsfile);//数组版
+//		BuildIndex.buildArray(keywordsfile);//数组版
 		ACA aca = BuildIndex.buildACA(keywordsfile);//ACA版
 		keywords = BuildIndex.keywords;
 //		for(String keyword: keywords) {
@@ -28,6 +29,13 @@ public class Main {
 		int[] results;
 //		results = Count.count(words, keywords);//数组版
 		results = Count.countUsingACA(words, keywords, aca);//ACA版
+		
+		time_ms = Preprocess.time_ms + BuildIndex.time_ms + Count.time_ms;
+		
+		System.out.println("代码总行数： " + Preprocess.lines + " 行");
+		System.out.println("总用时 " + time_ms + " ms");
+		
+		System.out.println("统计结果--------------------");
 		for(int i=0; i<keywords.length; i++) {
 			System.out.println(keywords[i] + ": " + results[i]);
 		}
